7b1d8e631a4ca35575d3d2027a0e61e49748800a
Ashar Ali Shafqat <asharalishafqat@gmail.com>
2025-07-14 22:58:01 +0500

    Update 11

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,14 +7,14 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router basename="/bizbridge-capital">
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
